163bfda539d70750ab67d17e289082c64037e4b6
Carolina del Corral <77272717+carol-dcf@users.noreply.github.com>
2021-12-11 23:39:15 +0100

    missing comments

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from streaming import Streaming
 
 if __name__ == "__main__":
     # create instance
-    sp3 = Streaming("cut_bbb.mp4")
+    sp3 = Streaming("bbb.mp4")
 
     # Interactive menu
     ex = 1
     while ( ex != 0 ):
         print("\033[1m\nChoose an exercise:\033[0m")
-        print("\t 1. Change video codec" + "\n\t 2. Compare video codecs" + "\n\t 3. Stream" + "\n\t 4. Choose IP" + "\n\t 0. Exit")
+        print("\t 1. Change video codec" + "\n\t 2. Compare video codecs" + "\n\t 3. Stream and Choose IP" + "\n\t 0. Exit")
 
         ex = int(input())
 
@@ -23,17 +23,14 @@ if __name__ == "__main__":
         #### 2 #####
         elif (ex == 2):
             print("\n\033[1mEXERCISE 2\033[0m")
-            sp3.compare_vcodecs("VP8", "VP9", "Results/VP8_cut_bbb.webm", "Results/VP9_cut_bbb.webm")
+            sp3.compare_vcodecs("VP8", "VP9", "Results/VP8_720p_bbb.webm", "Results/VP9_720p_bbb.webm")
 
 
         elif (ex == 3):
             #### 3 #####
             print("\n\033[1mEXERCISE 3\033[0m")
-            sp3.stream2VLC("rtp://92.178.211.111:9999")
-
-        elif (ex == 4):
-            #### 4 #####
-            print("\n\033[1mEXERCISE 4\033[0m")
+            ip = sp3.choose_ip()
+            sp3.stream2VLC(ip)
 
         elif (ex == 0):
             print("Application closed.")
--- streaming.py
+++ streaming.py
@@ -5,23 +5,31 @@ from math import sin
 class Streaming:
     def __init__(self, input_path):
         self.input_path = input_path
+        self.input_path_720p = "720p_" + input_path
+        self.input_path_480p = "480p_" + input_path
+        self.input_path_240p = "240p_" + input_path
+        self.input_path_120p = "120p_" + input_path
+        self.other_input_paths = [self.input_path_720p, self.input_path_480p, self.input_path_240p, self.input_path_120p]
         self.output_folder = "Results/"
         if not os.path.exists(self.output_folder):
             os.mkdir(self.output_folder)
 
     # exercise 1: change video codedc
     def change_codec(self, new_codec):
-        output_path = self.output_folder + new_codec + "_" + self.input_path.split(".")[0]
-        if new_codec == "VP8":
-            codec_command = "ffmpeg -i " + self.input_path + " -c:v libvpx -b:v 1M -c:a libvorbis " + output_path + ".webm"
-        elif new_codec == "VP9":
-            codec_command = "ffmpeg -i " + self.input_path + " -c:v libvpx-vp9 -b:v 2M " + output_path + ".webm"
-        elif new_codec == "h265":
-            codec_command = "ffmpeg -i " + self.input_path + " -c:v libx265 -crf 26 -preset fast -c:a aac -b:a 128k " + output_path + ".mp4"
-        elif new_codec == "AV1":
-            codec_command = "ffmpeg -i " + self.input_path + " -c:v libaom-av1 -crf 30 -b:v 0 " + output_path + ".mkv"
-        print(codec_command)
-        os.system(codec_command)
+        output_paths = []
+        for input in self.other_input_paths:
+            output_path = self.output_folder + new_codec + "_" + input.split(".")[0]
+            output_paths.append(output_path)
+            if new_codec == "VP8":
+                codec_command = "ffmpeg -i " + input + " -c:v libvpx -b:v 1M -c:a libvorbis " + output_path + ".webm"
+            elif new_codec == "VP9":
+                codec_command = "ffmpeg -i " + input + " -c:v libvpx-vp9 -b:v 2M " + output_path + ".webm"
+            elif new_codec == "h265":
+                codec_command = "ffmpeg -i " + input + " -c:v libx265 -crf 26 -preset fast -c:a aac -b:a 128k " + output_path + ".mp4"
+            elif new_codec == "AV1":
+                codec_command = "ffmpeg -i " + input + " -c:v libaom-av1 -crf 30 -b:v 0 " + output_path + ".mkv"
+            print(codec_command)
+            os.system(codec_command)
         return output_path
 
     # exercise 2: compare video codecs
@@ -34,11 +42,15 @@ class Streaming:
 
     # exercise 3: live streaming
     def stream2VLC(self, ip):
-        stream_command = "ffmpeg -re -i " + "120p_bbb.mp4" + " -f flv udp://localhost:23000"
+        stream_command = "ffmpeg -re -i " + self.input_path + " -f flv udp://" + ip
+        print("\n" + stream_command + "\n")
         os.system(stream_command)
         return
 
     # exercise 4: choose IP
     def choose_ip(self):
-        ip = str(input("Input your IP address: "))
-        return ip
+        ip = str(input("Input your IP address (eg. 127.0.0.1): "))
+        port = str(input("Input your port (eg. 23000): "))
+        final_ip = ip + ":" + port
+        print("Your chosen IP address to broadcast to is: ", final_ip)
+        return final_ip
